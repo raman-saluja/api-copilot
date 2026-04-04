b3766896282093c578a488ea59989bc1b0bc8d26
Raman <ramansaluja849@gmail.com>
2026-04-05 00:24:32 +0530

    removed extra spacing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import SwaggerView from "./views/SwaggerView";
 function App() {
   return (
     <Router>
-      <div className="py-10 min-h-screen w-screen bg-[radial-gradient(circle_at_top_left,rgba(99,102,241,0.15),transparent_40%),radial-gradient(circle_at_bottom_right,rgba(236,72,153,0.15),transparent_40%)]">
+      <div className="min-h-screen w-screen bg-[radial-gradient(circle_at_top_left,rgba(99,102,241,0.15),transparent_40%),radial-gradient(circle_at_bottom_right,rgba(236,72,153,0.15),transparent_40%)]">
         {/* Global Toaster for notifications */}
         <Toaster
           position="top-right"
